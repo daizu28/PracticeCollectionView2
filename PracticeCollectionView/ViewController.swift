ff6fe4281021878b12168ea9da9a9791ae24b2a1
小林玲衣奈 <kinakodaizukinako@gmail.com>
2020-10-01 22:28:36 +0900

    flowlayoutの練習

--- PracticeCollectionView/ViewController.swift
+++ PracticeCollectionView/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    
+    //コレクションビューの宣言？Flowlayoutには必要なのかな？
+    @IBOutlet weak var collectionView: UICollectionView!
     
     private let images = ["checkedTask", "emptyTask", "enterTask"]
     
@@ -25,6 +26,20 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        collectionView.dataSource = self
+
+
+          // レイアウト設定(デフォルトでのセルの幅は50らしい)
+          let layout = UICollectionViewFlowLayout()
+          layout.itemSize = CGSize(width: 100, height: 100)
+          collectionView.collectionViewLayout = layout
+        //行の最後のセルがふちにぴったりとくっつく(横の余白)
+        layout.minimumInteritemSpacing = 10
+        //縦の余白
+        layout.minimumLineSpacing = 50
+        //セクションごとのマージンみたいな
+        layout.sectionInset = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
+
     }
     
     
@@ -48,28 +63,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return cell
     }
     
-    // Screenサイズに応じたセルサイズを返す
-       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        //paddingだからセル同士の隙間？
-           let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
-        //padding分を引いてる？
-           let availableWidth = view.frame.width - paddingSpace
-        //padding分を引いてアイテム数で割ってるのが横幅になる？
-           let widthPerItem = availableWidth / itemsPerRow
-        //縦幅と横幅が同じということ
-           return CGSize(width: widthPerItem, height: widthPerItem)
-       }
-
-
-       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-           return sectionInsets
-       }
-
-       // セルの行間の設定
-       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-           return 5.0
-       }
+    // セクションを増やす(これを横に増やせればできそう？)
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
     
     // セルが選択されたときの処理
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -78,3 +76,27 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
 }
 
+
+//本当は良くないかもだけどgithubで上手にブランチが切れてなかった用のコメントアウト
+//    // Screenサイズに応じたセルサイズを返す
+//       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        //paddingだからセル同士の隙間？
+//           let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+//        //padding分を引いてる？
+//           let availableWidth = view.frame.width - paddingSpace
+//        //padding分を引いてアイテム数で割ってるのが横幅になる？
+//           let widthPerItem = availableWidth / itemsPerRow
+//        //縦幅と横幅が同じということ
+//           return CGSize(width: widthPerItem, height: widthPerItem)
+//       }
+//
+//
+//       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//           return sectionInsets
+//       }
+//
+//       // セルの行間の設定
+//       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//           return 5.0
+//       }
